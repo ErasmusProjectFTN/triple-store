ebdcd93fda203bfdc82b020f3b51f19c53db86f4
rasajski <vwm0w0mwv@gmail.com>
2017-02-12 18:47:41 +0100

    Initial commit

---
+++ src/main/java/com/ErasmusProject/TripleStoreServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ErasmusProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleStoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleStoreServiceApplication.class, args);
+	}
+}
