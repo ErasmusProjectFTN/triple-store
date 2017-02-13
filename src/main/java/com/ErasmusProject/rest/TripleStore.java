b31bb26c4866f2958e54ba1549f9fc35438e0037
rasajski <vwm0w0mwv@gmail.com>
2017-02-13 15:05:04 +0100

    Student add endpoint defined

--- src/main/java/com/ErasmusProject/rest/TripleStore.java
+++ src/main/java/com/ErasmusProject/rest/TripleStore.java
@@ -1,6 +1,7 @@
 package com.ErasmusProject.rest;
 import com.ErasmusProject.util.Response;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 /**
@@ -8,10 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping("/add")
 public class TripleStore {
-    @RequestMapping("/greeting")
-    public Response greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Response("Hello", name);
+
+    @RequestMapping(method = RequestMethod.POST, value = "/student")
+    public Response addStudent(@RequestParam("surname") String surname,
+                               @RequestParam("name") String name,
+                               @RequestParam("dateOfBirth") String dob,
+                               @RequestParam("countryofBirth") String cob,
+                               @RequestParam("placeOfBirth") String pob,
+                               @RequestParam("gender") String gender,
+                               @RequestParam("nationality") String nat,
+                               @RequestParam("streetAndNumber") String san,
+                               @RequestParam("postalCode") String pcode,
+                               @RequestParam("city") String city,
+                               @RequestParam("country") String cor,
+                               @RequestParam("telephone") String tel,
+                               @RequestParam("email") String email)
+    {
+        return new Response("Hello", "World");
     }
 
 }
