47e551cd37a1782d9d0068d5ced1ce0ce7a30e13
Aleksandar Lukic <Aleksandar Lukic>
2017-02-19 19:36:11 +0100

    Added service discovery configuration

--- src/main/java/com/ErasmusProject/app/TripleStoreServiceApplication.java
+++ src/main/java/com/ErasmusProject/app/TripleStoreServiceApplication.java
@@ -2,8 +2,10 @@ package com.ErasmusProject.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.ErasmusProject.app","com.ErasmusProject.rest"})
 public class TripleStoreServiceApplication {
